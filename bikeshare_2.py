88529d39488cc2927e70f45efcfeeb668f74d0f0
Adolin Novaes-Card <novaescaa@leidos.com>
2024-08-20 12:52:59 -0400

    Add user prompt for City

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,7 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    city = print('Would you like to look at data in Chicago, New York City, or Washington?')
 
     # get user input for month (all, january, february, ... , june)
 
